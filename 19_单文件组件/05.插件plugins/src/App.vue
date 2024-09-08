bf2dc6db058d170b3951189e8d5d0f9293f42f39
jack-luo-p <3049212272@qq.com>
2024-09-08 14:45:24 +0800

    add: 添加插件代码

---
+++ 19_单文件组件/05.插件plugins/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <student></student>
+        <hr>
+        <school></school>
+    </div>
+</template>
+<script>
+// 引入组件
+import student from './components/Student.vue';
+import school from './components/School.vue';
+
+export default {
+    name: 'App',
+    components: {
+        student,
+        school
+    }
+}
+</script>
