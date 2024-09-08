8b86ab5032d28c7ecfd4a06978b9d1f1edc2e9ac
jack-luo-p <3049212272@qq.com>
2024-09-08 09:41:49 +0800

    添加ref属性代码

---
+++ 19_单文件组件/ref属性/App.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <school></school>
+        <student></student>
+    </div>
+</template>
+<script>
+// 引入组件
+import school from './components/School.vue';
+import student from './components/Student.vue';
+
+export default {
+    name: 'App',
+    components: {
+        school,
+        student
+    }
+}
+</script>
---
+++ 19_单文件组件/ref属性/components/Student.vue
@@ -0,0 +1,25 @@
+<template>
+    <!-- 组件的结构 -->
+    <div class="demo">
+        <h2>学生姓名: {{ name }}</h2>
+        <h2>学生地址: {{ age }}</h2>
+        <button @click="showName">点我提示学校名</button>
+    </div>
+</template>
+<script>
+// 交互相关代码
+export default {
+    name: 'Student',
+    data() {
+        return {
+            name: '张三',
+            age: 18
+        }
+    },
+    methods: {
+        showName() {
+            alert(this.name)
+        }
+    }
+}
+</script>
