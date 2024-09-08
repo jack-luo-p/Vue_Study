dde18ad634c4b7ba7f374dfebacc7283648bf975
jack-luo-p <3049212272@qq.com>
2024-09-08 13:59:08 +0800

    add: 添加mixins（混入）配置项

--- 19_单文件组件/04.mixin混入(合)/src/components/School.vue
+++ 19_单文件组件/04.mixin混入(合)/src/components/School.vue
@@ -1,20 +1,20 @@
 <template>
     <!-- 组件的结构 -->
     <div>
-        <h2 @click="showName">学生姓名: {{ name }}</h2>
-        <h2>学生性别: {{ sex }}</h2>
+        <h2 @click="showName">学习名称: {{ name }}</h2>
+        <h2>学校地址: {{ address }}</h2>
     </div>
 </template>
 <script>
-// 引入一个混合
+// 引入一个混合(局部)
 // import {mixin,mixin2} from '../mixin'
 // 交互相关代码
 export default {
     name: 'Student',
     data() {
         return { 
-            name: '张三',
-            sex: '男'
+            name: '尚硅谷',
+            address: '北京'
         }
     },
     // mixins: [mixin,mixin2]
