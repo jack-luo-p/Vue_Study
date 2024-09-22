ce9b3a7499908b0cbc3aa8f00f979e6dadd87be2
jack-luo-p <3049212272@qq.com>
2024-09-22 18:39:16 +0800

    add: 添加组件自定义事件(触发事件this.$emit('atguigu',数据);组件可以绑定原生DOM事件,需要使用native修饰符)

---
+++ 19_单文件组件/09.组件自定义事件/src/components/School.vue
@@ -0,0 +1,32 @@
+<template>
+    <!-- 组件的结构 -->
+    <div class="school">
+        <h2>学习名称: {{ name }}</h2>
+        <h2>学校地址: {{ address }}</h2>
+        <button @click="sendSchoolName">把学校名给App</button>
+    </div>
+</template>
+<script>
+export default {
+    name: 'Student',
+    data() {
+        return { 
+            name: '尚硅谷atguigu',
+            address: '北京'
+        }
+    },
+    props:['getSchoolName'],
+    methods: {
+        sendSchoolName() {
+            this.getSchoolName(this.name)
+        }
+    }
+}
+</script>
+
+<style lang="less" scoped>
+    .school {
+        background-color: skyblue;
+        padding: 5px;
+    }
+</style>
---
+++ 19_单文件组件/vue_test/src/components/Student.vue
@@ -0,0 +1,51 @@
+<template>
+    <!-- 组件的结构 -->
+    <div class="student">
+        <h2>学生姓名: {{ name }}</h2>
+        <h2>学生性别: {{ sex }}</h2>
+        <h2>当前求和为： {{ number }}</h2>
+        <button @click=add>点我number++</button>
+        <button @click="sendStudentName">把学生名给App</button>
+        <button @click="unbind">解绑atguigu事件</button>
+        <button @click="death">销毁当前Student组件的实例(vc)</button>
+    </div>
+</template>
+<script>
+export default {
+    name: 'Student',
+    data() {
+        return { 
+            name: '张三',
+            sex: '男',
+            number: 0
+        }
+    },
+    methods: {
+        sendStudentName(){
+            // 触发Student组件实例身上的atguigu事件
+            this.$emit('atguigu',this.name)
+            // this.$emit('demo')
+        },
+        unbind() {
+            this.$off('atguigu') // 解绑一个自定义事件
+            // this.$off(['atguigu','demo']) // 解绑多个自定义事件
+            // this.$off() // 解绑所有自定义事件
+        },
+        death() {
+            this.$destroy() // 销毁了当前Student组件的实例,销毁后所有的Student实例的自定义事件全都不奏效
+        },
+        add() {
+            console.log('add回调被调用了')
+            this.number++
+        }
+    }
+}
+</script>
+
+<style lang="less" scoped>
+    .student {
+        background-color: pink;
+        padding: 5px;
+        margin-top: 30px;
+    }
+</style>
