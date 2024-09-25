939a3f607ec311e271697b0755ab99860becb777
jack-luo-p <3049212272@qq.com>
2024-09-26 00:12:08 +0800

    add:添加全局事件总线

---
+++ 19_单文件组件/11.全局事件总线/src/components/Student.vue
@@ -0,0 +1,38 @@
+<template>
+    <!-- 组件的结构 -->
+    <div class="student">
+        <h2>学生姓名: {{ name }}</h2>
+        <h2>学生性别: {{ sex }}</h2>
+        <button @click="sendStudentName">把学生名给school组件</button>
+    </div>
+</template>
+<script>
+export default {
+    name: 'Student',
+    data() {
+        return { 
+            name: '张三',
+            sex: '男',
+        }
+    },
+    mounted() {
+        
+    },
+    methods:{
+        sendStudentName() {
+            this.$bus.$emit('hello',this.name)
+        }
+    },
+    beforeDestroy() {
+        this.$bus.$off('hello')
+    }
+}
+</script>
+
+<style lang="less" scoped>
+    .student {
+        background-color: pink;
+        padding: 5px;
+        margin-top: 30px;
+    }
+</style>
---
+++ 19_单文件组件/vue_test/src/components/School.vue
@@ -0,0 +1,31 @@
+<template>
+    <!-- 组件的结构 -->
+    <div class="school">
+        <h2 ref="school">学习名称: {{ name }}</h2>
+        <h2>学校地址: {{ address }}</h2>
+    </div>
+</template>
+<script>
+export default {
+    name: 'Student',
+    data() {
+        return { 
+            name: '尚硅谷atguigu',
+            address: '北京'
+        }
+    },
+    mounted() {
+        // console.log('School',this.x);
+        this.$bus.$on('hello',(data) => {
+            console.log('我是School组件,收到了数据',data);
+        })
+    }
+}
+</script>
+
+<style lang="less" scoped>
+    .school {
+        background-color: skyblue;
+        padding: 5px;
+    }
+</style>
